f5fda5fa8b9d82bea8e3fce2f13b0de32f3554dc
TheoMugnier <theo.mugnier@minesdedouai.fr>
2018-04-11 16:57:08 +0200

    Create ros-turtlebot-mapping.bash
    This script creates the needed container and opens a xhost access for this container.
    Then it starts the container.
    Finally when the container is stopped it closes the xhost access.

---
+++ ROS-Mapping/ros-turtlebot-mapping.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Creating the container from the ros:mapping image, it uses a turtlebot to map a room
+#/dev/ttyACM0 is the laser; /dev/input/js0 is the joystick; /dev/kobuki is the turtlebot
+sudo docker container create -it \
+    --device=/dev/ttyACM0 --device=/dev/input/js0 --device=/dev/kobuki \
+    --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    ros:mapping \
+    roslaunch adaptive_local_planner simple_exploration_karto.launch
+
+#Opening up xhost to the container, allowing rviz to start its GUI
+containerId=$(sudo docker container ps -l -q)
+xhost +local:`sudo docker inspect --format='{{ .Config.Hostname }}' $containerId`
+
+#Starting the container in interactive mode
+sudo docker container start -i --rm $containerId
+
+#Closing xhost access opened for the container
+xhost -local:`sudo docker inspect --format='{{ .Config.Hostname }}' $containerId`
