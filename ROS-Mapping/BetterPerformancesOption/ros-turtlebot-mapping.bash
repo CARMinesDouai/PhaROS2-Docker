1f47c924956cf0f556508cb90d05a782953efbbd
TheoMugnier <theo.mugnier@minesdedouai.fr>
2018-04-23 17:13:05 +0200

    Now uses the correct RVIZ configuration

--- ROS-Mapping/BetterPerformancesOption/ros-turtlebot-mapping.bash
+++ ROS-Mapping/BetterPerformancesOption/ros-turtlebot-mapping.bash
@@ -14,9 +14,13 @@ sudo docker run -d --rm \
     ros:mapping \
     roslaunch adaptive_local_planner simple_exploration_karto.launch
 containerId=$(sudo docker container ps -l -q --no-trunc)
+echo "Launching turtlebot mapping..."
+sleep 10s #to allow launch file to start mapping before we run RVIZ 
 
-#Launching RVIZ on the host
-ROS_MASTER_URI=http://$containerIP:11311 rviz
+#Launching RVIZ on the host (with the correct configuration)
+sudo docker cp $containerId:/root/rosws/src/adaptive_local_planner/rviz/robot_navigation.rviz /tmp/config.rviz
+ROS_MASTER_URI=http://$containerIP:11311 rviz -d /tmp/config.rviz
+sudo rm /tmp/config.rviz
 
 #When user exit RVIZ the map will be saved, then the container will stop and be removed
 ROS_MASTER_URI=http://$containerIP:11311 rosrun map_server map_saver -f /tmp/ros_map
