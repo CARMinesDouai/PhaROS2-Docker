483cfe1b1bacdb843cdef44eab30f730ef562e84
TheoMugnier <theo.mugnier@minesdedouai.fr>
2018-04-25 16:52:41 +0200

    Transfering the map to the container

--- ROS-Navigation/ros-turtlebot-navigation-in-map.bash
+++ ROS-Navigation/ros-turtlebot-navigation-in-map.bash
@@ -1,20 +1,25 @@
 #!/bin/bash
 if [ $# -ne 1 ]
  then
-  echo "This script needs one argument : the path to the premade map.yaml"
+  echo -e "\e[1;31mERROR ! This script needs one argument : the path to the premade map (without any extension).\e[0m"
+  return 1
 fi
 
-#Creating the container using a turtlebot to navigate in the room
-#/dev/ttyACM0 is the laser; /dev/input/js0 is the joystick; /dev/kobuki is the turtlebot
+#Creating the container using a turtlebot to navigate in the map
+#/dev/ttyACM0 is the laser; /dev/kobuki is the turtlebot
 sudo docker container create -it \
-    --device=/dev/ttyACM0 --device=/dev/input/js0 --device=/dev/kobuki \
+    --device=/dev/ttyACM0 --device=/dev/kobuki \
     --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --env TURTLEBOT_MAP_FILE=$1 \
+    --env TURTLEBOT_MAP_FILE=/tmp/turtlebot_map.yaml \
     ros:navigation-in-map \
     roslaunch adaptive_local_planner turtlebot_navigation_in_map.launch
+containerId=$(sudo docker container ps -l -q --no-trunc)
+
+#Transfering the map to the container
+sudo docker cp $1.yaml $containerId:/tmp/turtlebot_map.yaml
+sudo docker cp $1.pgm $containerId:/tmp/turtlebot_map.pgm
 
 #Opening up xhost to the container, allowing rviz to start its GUI
-containerId=$(sudo docker container ps -l -q --no-trunc)
 xhost +local:`sudo docker inspect --format='{{ .Config.Hostname }}' $containerId`
 
 #Starting the container in interactive mode
