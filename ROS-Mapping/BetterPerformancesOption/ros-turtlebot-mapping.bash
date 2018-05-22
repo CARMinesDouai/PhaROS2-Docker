b1afbdcf20a2017e24c139252d7107d79bd34ad4
TheoMugnier <theo.mugnier@minesdedouai.fr>
2018-05-22 15:15:35 +0200

    Adding autonomous option and updating comments

--- ROS-Mapping/BetterPerformancesOption/ros-turtlebot-mapping.bash
+++ ROS-Mapping/BetterPerformancesOption/ros-turtlebot-mapping.bash
@@ -1,4 +1,15 @@
-#!/bin/bash
+#!/bin/bash 
+#This script will use the ros:mapping docker image to map a room with a turtlebot, a laser and a joystick
+
+#By default this script uses a teleop mapping launchfile, but can also use an autonomous one (not maintained)
+launchfile=simple_exploration_karto.launch
+while getopts "a" opt; do
+	case $opt in
+		a)
+			launchfile=autonomous_single_robot_exploration_karto.launch
+			;;
+	esac
+done
 
 #Getting the IP of the future container (by running a temporary container)
 sudo docker run -d --rm ros:mapping
@@ -6,13 +17,13 @@ containerId=$(sudo docker container ps -l -q --no-trunc)
 containerIP=$(sudo docker container inspect --format '{{ .NetworkSettings.IPAddress }}' $containerId)
 sudo docker container stop $containerId
 
-#Creating the container using a turtlebot to map the room
+#Creating the docker container
 #/dev/ttyACM0 is the laser; /dev/input/js0 is the joystick; /dev/kobuki is the turtlebot
 sudo docker run -d --rm \
     --device=/dev/ttyACM0 --device=/dev/input/js0 --device=/dev/kobuki \
     --env ROS_IP=$containerIP \
     ros:mapping \
-    roslaunch adaptive_local_planner simple_exploration_karto.launch
+    roslaunch adaptive_local_planner $launchfile
 containerId=$(sudo docker container ps -l -q --no-trunc)
 echo "Launching turtlebot mapping..."
 sleep 10s #to allow launch file to start mapping before we run RVIZ 
