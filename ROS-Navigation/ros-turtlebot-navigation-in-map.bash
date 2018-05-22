2b6d78369a331f6cbb710f88066843e5c8305e3a
TheoMugnier <theo.mugnier@minesdedouai.fr>
2018-05-22 15:18:17 +0200

    Auto removing of the container + commenting

--- ROS-Navigation/ros-turtlebot-navigation-in-map.bash
+++ ROS-Navigation/ros-turtlebot-navigation-in-map.bash
@@ -1,13 +1,16 @@
 #!/bin/bash
+#This script will use the ros:navigation-in-map docker image to navigate in a already mapped room with a turtlebot and a laser
+
+#If this script isn't called with exactly one argument, it will return an error message specifying its requirement
 if [ $# -ne 1 ]
  then
   echo -e "\e[1;31mERROR ! This script needs one argument : the path to the premade map (without any extension).\e[0m"
   return 1
 fi
 
-#Creating the container using a turtlebot to navigate in the map
+#Creating the container
 #/dev/ttyACM0 is the laser; /dev/kobuki is the turtlebot
-sudo docker container create -it \
+sudo docker container create -it --rm \
     --device=/dev/ttyACM0 --device=/dev/kobuki \
     --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --env TURTLEBOT_MAP_FILE=/tmp/turtlebot_map.yaml \
@@ -27,6 +30,3 @@ sudo docker container start -i $containerId
 
 #Closing xhost access opened for the container
 xhost -local:`sudo docker inspect --format='{{ .Config.Hostname }}' $containerId`
-
-#Removing container
-sudo docker container rm $containerId
